cc9476535bff2d3a68274b8d2ceb7f84b0dce054
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-08-24 22:59:35 -0700

    Add `virtio_pci` to loaded modules; leave only `-virtfs`
    Guest kernel may not have `virtio_pci` module loaded by default, so load
    it explicitly. Also, `-virtfs` is a synonym to `-device 'virtio-9p-pci'`
    so the latter is redundant and can be removed.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- initramfs_builder/run.sh
+++ initramfs_builder/run.sh
@@ -15,7 +15,6 @@ exec qemu-system-x86_64 \
     -m 1G \
     -append "console=ttyS0 loglevel=3 quiet oops=panic $*" \
     -device virtio-rng-pci \
-    -virtfs 'local,path=/,id=hostfs,mount_tag=hostfs,security_model=none,readonly=on' \
-    -device 'virtio-9p-pci,fsdev=hostfs,mount_tag=hostfs' \
+    -virtfs local,path=/,id=hostfs,mount_tag=hostfs,security_model=none,readonly \
     -object memory-backend-epc,id=epc_mem,size=64M,prealloc=on \
     -M sgx-epc.0.memdev=epc_mem
